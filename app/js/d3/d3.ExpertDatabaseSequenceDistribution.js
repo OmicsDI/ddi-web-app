e2d0d636fe8628a0e6ae18ec5566e1b954ab3017
baimingze <baimingze@gmail.com>
2015-04-08 14:00:14 +0100

    browse page and other js codes added

---
+++ app/js/d3/d3.ExpertDatabaseSequenceDistribution.js
@@ -0,0 +1,127 @@
+/*
+Copyright [2009-2014] EMBL-European Bioinformatics Institute
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Visualize expert database sequence distribution as an area chart.
+// Inspired by:
+// http://bl.ocks.org/mbostock/3883195
+// http://bl.ocks.org/mbostock/3902569
+
+ExpertDatabaseSequenceDistribution = function(selector, data, max_width, max_height){
+
+    var margin = {top: 20, right: 60, bottom: 40, left: 50},
+        width = max_width - margin.left - margin.right,
+        height = max_height - margin.top - margin.bottom;
+
+    var bisect = d3.bisector(function(d) { return d.length; }).left;
+
+    var x = d3.scale.log()
+        .range([0, width]);
+
+    var y = d3.scale.log()
+        .range([height, 0]);
+
+    var numberFormat = d3.format("s");
+    function logFormat(d) {
+        var x = Math.log(d) / Math.log(10) + 1e-6;
+        return Math.abs(x - Math.floor(x)) < .6 ? numberFormat(d) : "";
+    }
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .tickFormat(logFormat)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .tickFormat(logFormat)
+        .orient("left");
+
+    var area = d3.svg.area()
+        .x(function(d) { return x(d.length); })
+        .interpolate("monotone")
+        .y0(height)
+        .y1(function(d) { return y(d.count); });
+
+    var svg = d3.select(selector).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    data.forEach(function(d) {
+      d.length = d.length;
+      d.count = +d.count;
+    });
+
+    data.sort(function(a, b) {
+        return a.length - b.length;
+    });
+
+    x.domain(d3.extent(data, function(d) { return d.length; }));
+    y.domain(d3.extent(data, function(d) { return d.count; }));
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+      .append("text")
+        .attr("x", 330)
+        .attr("dy", "2.7em")
+        .style("text-anchor", "middle")
+        .text("Number of nucleotides");
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Sequences");
+
+    svg.append("path")
+        .datum(data)
+        .attr("class", "area")
+        .attr("d", area);
+
+    var focus = svg.append("g")
+      .attr("class", "focus")
+      .style("display", "none");
+
+    focus.append("circle")
+      .attr("r", 4.5);
+
+    focus.append("text")
+      .attr("x", 9)
+      .attr("dy", ".35em");
+
+    svg.append("rect")
+      .attr("class", "overlay")
+      .attr("width", width)
+      .attr("height", height)
+      .on("mouseover", function() { focus.style("display", null); })
+      .on("mouseout", function() { focus.style("display", "none"); })
+      .on("mousemove", mousemove);
+
+    function mousemove() {
+        var x0 = x.invert(d3.mouse(this)[0]),
+            i = bisect(data, x0, 1),
+            d0 = data[i - 1],
+            d1 = data[Math.min(i + 1, data.length - 1)],
+            d = x0 - d0.count > d1.count - x0 ? d1 : d0;
+        focus.attr("transform", "translate(" + x(d.length) + "," + y(d.count) + ")");
+        focus.select("text").text(d.length + " nts");
+    }
+
+}
---
+++ app/js/d3/d3.speciesSunburst.js
@@ -0,0 +1,129 @@
+/*
+Copyright [2009-2014] EMBL-European Bioinformatics Institute
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Displays species sunburst based on the combined lineages from all xrefs.
+// Based on http://bl.ocks.org/mbostock/4348373
+// Text labels: http://blog.luzid.com/2013/extending-the-d3-zoomable-sunburst-with-labels/
+
+d3SpeciesSunburst = function(data, selector, width, height){
+
+	var jsonTree = data,
+	    radius = Math.min(width, height) / 2;
+
+	var x = d3.scale.linear()
+	    .range([0, 2 * Math.PI]);
+
+	var y = d3.scale.linear()
+	    .range([0, radius]);
+
+	var color = d3.scale.category20c();
+
+	var svg = d3.select(selector).append("svg")
+	    .attr("width", width)
+	    .attr("height", height)
+	  .append("g")
+	    .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
+
+	var partition = d3.layout.partition()
+	    .value(function(d) { return d.size; });
+
+	var arc = d3.svg.arc()
+	    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
+	    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
+	    .innerRadius(function(d) { return Math.max(0, y(d.y)); })
+	    .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
+
+	// d3.json("flare.json", function(error, root) {
+	  var g = svg.selectAll("g")
+	      .data(partition.nodes(jsonTree))
+	    .enter().append("g");
+
+	  // show the number of sequences for terminal nodes
+	  var get_title = function(d){
+	    var title = d.name;
+	    if (d.hasOwnProperty('size')) {
+	      title += " (" + d.size + " sequences)";
+	    }
+	    return title;
+	  };
+
+	  var path = g.append("path")
+	    .attr("d", arc)
+	    // .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
+	    .attr("fill", function(d) { return "#6baed6"; })
+	    .attr("title", function(d) { return get_title(d); })
+		.on("mouseover", function() {
+		  d3.select(this)
+		    .attr('fill', '#f0ad4e')
+		})
+		.on("mouseout",  function() {
+		  d3.select(this)
+		    .attr('fill', '#6baed6' )
+	  	});
+
+	    ;
+	    // .on("click", click);
+
+	  // var text = g.append("text")
+	  //   .attr("transform", function(d) { return "rotate(" + computeTextRotation(d) + ")"; })
+	  //   .attr("x", function(d) { return y(d.y); })
+	  //   .attr("dx", "6") // margin
+	  //   .attr("dy", ".35em") // vertical-align
+	  //   .text(function(d) { return d.name; });
+
+	  // function click(d) {
+	  //   // fade out all text elements
+	  //   text.transition().attr("opacity", 0);
+
+	  //   path.transition()
+	  //     .duration(750)
+	  //     .attrTween("d", arcTween(d))
+	  //     .each("end", function(e, i) {
+	  //         // check if the animated element's data e lies within the visible angle span given in d
+	  //         if (e.x >= d.x && e.x < (d.x + d.dx)) {
+	  //           // get a selection of the associated text element
+	  //           var arcText = d3.select(this.parentNode).select("text");
+	  //           // fade in the text element and recalculate positions
+	  //           arcText.transition().duration(0)
+	  //             .attr("opacity", 1)
+	  //             .attr("transform", function() { return "rotate(" + computeTextRotation(e) + ")" })
+	  //             .attr("x", function(d) { return y(d.y); });
+	  //         }
+	  //     });
+	  // }
+	// });
+
+	d3.select(self.frameElement).style("height", height + "px");
+
+	//initializing tooltips
+	$("svg path").tooltip({
+	    'container': 'body'
+	});
+
+	// Interpolate the scales!
+	function arcTween(d) {
+	  var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
+	      yd = d3.interpolate(y.domain(), [d.y, 1]),
+	      yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
+	  return function(d, i) {
+	    return i
+	        ? function(t) { return arc(d); }
+	        : function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
+	  };
+	}
+
+	function computeTextRotation(d) {
+	  return (x(d.x + d.dx / 2) - Math.PI / 2) / Math.PI * 180;
+	}
+
+};
