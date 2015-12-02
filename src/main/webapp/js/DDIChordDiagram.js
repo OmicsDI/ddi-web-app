35716e408d94b3ddd92f6fe6e610dc016db073a3
Mingze <mingze@localhost.localdomain>
2015-12-02 14:22:00 +0000

    changed minimumThreshold to 0.5

--- src/main/webapp/js/DDIChordDiagram.js
+++ src/main/webapp/js/DDIChordDiagram.js
@@ -1,7 +1,7 @@
 /**
  * Created by mingze on 14/10/15.
  */
-function drawChordDiagram(acc, domain) {
+function drawChordDiagram() {
     var urlString = window.location.hash;
     var urlWords = urlString.split("/");
     var acc = urlWords[3];
@@ -9,6 +9,7 @@ function drawChordDiagram(acc, domain) {
     if(domain  == 'metabolomics_workbench'){
         domain ='MetabolomicsWorkbench';
     }
+    minimumThreshold = 0.5;
     queue()
         .defer(d3.json, web_service_url + 'enrichment/getSimilarityInfo?accession=' + acc+ '&database=' + domain) // topojson polygons
         //.defer(d3.json, 'http://localhost:9091/' + 'enrichment/getSimilarityInfo?accession=' + acc + '&database=' + domain) // topojson polygons
@@ -18,6 +19,11 @@ function drawChordDiagram(acc, domain) {
     function drawTheChord(error, similarityData) {
 
         if(similarityData.scores.length < 1 ){
+            return;
+        };
+
+
+        if(!findAScoreBiggerThan(similarityData.scores,minimumThreshold)){
             return;
         };
 
@@ -179,5 +185,18 @@ function drawChordDiagram(acc, domain) {
                 scores[minIndex] = tempScore;
             }
         }
+
+        function findAScoreBiggerThan(scores, minimumThreshold) {
+            var main_key = acc + "@" + domain;
+            for(var i=0; i<scores.length; i++) {
+                var key1 = scores[i].key1;
+                var key2 = scores[i].key2;
+                if(scores[i].value >= minimumThreshold && (key1 == main_key||key2 == main_key)){
+                    return true;
+                }
+            }
+            return false;
+        }
+
     }
 }
