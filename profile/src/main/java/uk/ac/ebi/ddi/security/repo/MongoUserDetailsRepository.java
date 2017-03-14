4f053055e6ed3a6f63e4cda70cf023bbde909684
andrey <helioraptor@gmail.com>
2017-03-14 13:17:18 +0000

    MongoUserDetailsRepository.java

---
+++ profile/src/main/java/uk/ac/ebi/ddi/security/repo/MongoUserDetailsRepository.java
@@ -0,0 +1,25 @@
+package uk.ac.ebi.ddi.security.repo;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import uk.ac.ebi.ddi.security.model.MongoUser;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Created by user on 3/13/2017.
+ */
+@Repository
+public interface MongoUserDetailsRepository extends PagingAndSortingRepository<MongoUser, String> {
+    @Query("{userName: ?0}")
+    MongoUser findByName(String name);
+
+    @Query("{UserId: ?0}")
+    MongoUser findByUserId(String UserId);
+
+    Page<MongoUser> findByUserIdNotNull(Pageable pageable);
+}
