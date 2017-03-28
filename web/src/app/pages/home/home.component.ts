04c9794e2e8c6bca776c3fbc7b8bfdcf210fc192
Ricardo-Ke <514068102fk@gmail.com>
2017-03-28 16:18:32 +0800

    add d3 npm module

--- web/src/app/pages/home/home.component.ts
+++ web/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  elementRef: ElementRef;
 
-  constructor() { }
+  private chartOmics: string;
+  private omicsBody: any;
 
-  ngOnInit() {
+  constructor() {
+    this.chartOmics = 'chart_repos_omics';
   }
 
+  ngOnInit() {
+    this.appendPara();
+  }
+
+  private appendPara() {
+    this.omicsBody = d3.select('#' + this.chartOmics)
+                        .append('g')
+                        .text('Hi this is text appended')
+                        .style('color', 'red');
+  }
 }
