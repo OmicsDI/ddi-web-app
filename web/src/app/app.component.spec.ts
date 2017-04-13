313e5893108093571ec891b1c83acd0c0efdd127
andrey <helioraptor@gmail.com>
2017-04-13 18:13:19 +0100

    app.component.spec.ts

--- web/src/app/app.component.spec.ts
+++ web/src/app/app.component.spec.ts
@@ -2,13 +2,40 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {RouterLinkStubDirective, RouterOutletStubComponent} from "../../testing/router-stubs";
+import {SlimLoadingBarComponent, SlimLoadingBarService} from "ng2-slim-loading-bar";
+import {AuthModule} from "angular2-jwt";
+import {AuthService} from "./services/auth.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ModuleWithProviders} from "@angular/core";
+import {routing} from "./app.routes";
+import {Observable} from "rxjs";
+
+let mockAuthService = {
+}
+let mockSlimLoadingBarService = {
+}
 
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        RouterLinkStubDirective, RouterOutletStubComponent, SlimLoadingBarComponent
       ],
+      providers: [
+        AuthService, SlimLoadingBarService,
+        {
+          provide: ActivatedRoute,
+          useValue: {queryParams: Observable.of({q: 'human'})}
+        },
+        {
+          provide: Router,
+          useValue: {
+            events: Observable.of({ url:'/home' })
+          }
+        }
+      ]
     });
     TestBed.compileComponents();
   });
@@ -19,16 +46,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'Omics DI 2.0'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('Omics DI 2.0');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h3').textContent).toContain('OmicsDI Databases');
   }));
 });
