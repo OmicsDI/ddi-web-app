0d4122062c31c152b342a2e38d5435951376915f
Ricardo-Ke <514068102fk@gmail.com>
2017-05-22 21:29:33 +0800

    database pictures show up

--- web/src/app/pages/database/database.component.ts
+++ web/src/app/pages/database/database.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
 import { DatabaseListService } from '../../services/database-list.service';
 import { Database } from '../../model/Database';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-database',
@@ -14,12 +15,14 @@ export class DatabaseComponent implements OnInit {
   public config = { 
     itemsPerPage: 8, 
     currentPage: this.p
-  }
+  };
+  public url: string;
 
   constructor(
     private databaseListService: DatabaseListService,
     private loadingService: SlimLoadingBarService) {
       this.loadingService.start();
+      this.url = environment.userServiceUrl;
     }
 
   ngOnInit() {
