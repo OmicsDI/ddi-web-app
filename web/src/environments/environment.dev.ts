50e2efb873b5a37b194eb09444cf32c2384afbde
Andrey Zorin <azorin@ebi.ac.uk>
2017-06-13 18:54:12 +0100

    dev environment = wwwdev

--- web/src/environments/environment.dev.ts
+++ web/src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   webServiceUrl : 'http://wwwdev.ebi.ac.uk/Tools/omicsdi/ws/',
-  userServiceUrl : 'http://ves-hx-43.ebi.ac.uk:8090/Tools/omicsdi/profilews/api/'
+  userServiceUrl : 'http://wwwdev.ebi.ac.uk/Tools/omicsdi/profilews/api/'
 };
