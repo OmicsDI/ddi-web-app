298732f7d17dda35265b5a83d457c116cbc09493
Ricardo-Ke <514068102fk@gmail.com>
2017-06-14 21:39:48 +0800

    tweets fixed

--- web/src/app/pages/home/charts/tweets-news/tweets-news.component.ts
+++ web/src/app/pages/home/charts/tweets-news/tweets-news.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Http, Response } from "@angular/http";
 import * as d3 from 'd3';
+import * as cheerio from 'cheerio';
 
 @Component({
   selector: 'app-tweets-news',
@@ -19,7 +20,10 @@ export class TweetsNewsComponent implements OnInit {
         .then(res => {
            this.notifyHomeLoader.emit('tweet_news');
            this.publishtweets(this.parseTweets(res, 4));
-      })
+        })
+        .catch(reason => {
+          console.warn(reason);
+        })
   }
 
   private fetchTweets(): Promise<Response> {
@@ -31,51 +35,55 @@ export class TweetsNewsComponent implements OnInit {
     return this.http.get(url)
       .toPromise();
   }
-  private parseTweets(res: Response, limit: number): any[] {
 
-    let _body: string = res["_body"];
-    _body = _body.substring(_body.indexOf("(") + 1, _body.length - 2);
-    let body = JSON.parse(_body)["body"];
-    let raw = document.createElement('div');
-    raw.innerHTML = body;
-    let tweets = raw.querySelectorAll('li.timeline-TweetList-tweet');
-    limit = (tweets.length < limit) ? tweets.length : limit;
-    let output = [];
-    let monthNamesShort = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  private parseTweets(res: Response, limit: number) {
+    const $ = cheerio.load(JSON.parse(res["_body"])["body"]);
+    let tweets = $("li.timeline-TweetList-tweet");
+    limit = tweets.length < limit ? tweets.length : limit;
+    let output = []
+      , t = {}
+      , monthNamesShort = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
     if (tweets.length == 0) {
       this.publishtweets([{ content: "Your Browser can not get our news from twitter service now.", id: "111", time: {} }]);
     }
-    for (let i = 0; i < limit; i++) {
-      let tr = tweets[i], t = {};
-      //console.log("tr:  "+tr);
-      t["id"] = tr.getAttribute('data-tweet-id');
-      let publishedtime = new Date(tr.querySelector('time.dt-updated').getAttribute('datetime'));
-      let publishedmonth = monthNamesShort[publishedtime.getMonth()];
-      let publishedday = publishedtime.getDate();
-      t["time"] = {
-        month: publishedmonth,
-        day: publishedday
-      }
-      let content = tr.querySelector('p.timeline-Tweet-text'),
-        tags = content.querySelectorAll('*');
-      for (let j = 0; j < tags.length; j++) {
-        let tag = tags[j], k = 0, del = [];
 
-        for (k = 0; k < tag.attributes.length; k++)
-          if (tag.attributes[k].name !== 'href')
-            del.push(tag.attributes[k].name);
+    $("li.timeline-TweetList-tweet")
+      .filter(function (i) {
+        return i < limit;
+      })
+      .each(function(i) {
+        let that = $(this);
+        t['id'] = that.find('.timeline-Tweet').data('tweet-id');
 
-        while (del.length)
-          tag.removeAttribute(del.pop());
-      }
+        let publishedtime = new Date(that.find('time.dt-updated').attr('datetime'))
+          , publishedmonth = monthNamesShort[publishedtime.getMonth()]
+          , publishedday = publishedtime.getDate();
 
-      t["content"] = content.innerHTML.replace(/<\/?b[^>]*>/gi, '');
-      output.push(t);
-    }
+        t["time"] = {
+          month: publishedmonth,
+          day: publishedday
+        }
 
+        let content = that.find('p.timeline-Tweet-text')
+          , tags = content.find('*');
+
+        tags.each(function(i, ele) {
+          let self = $(this), k = 0, del = [];
+          for (let m in ele.attribs) {
+            if (m != 'href') {
+              self.removeAttr(m);
+            }
+          }
+        })
+
+        t['content'] = content.html();
+        output.push(t);
+      });
+    
     return output;
   }
+
   private publishtweets(data: any[]): void {
     d3.select("#tweets-of-the-month").selectAll("div").remove();
     d3.select("#tweets-of-the-month").append("div").selectAll("div")
@@ -84,12 +92,14 @@ export class TweetsNewsComponent implements OnInit {
       .each(function () {
         let entrydiv = d3.select(this);
         entrydiv.attr("class", "row")
-          .attr("style", "margin-left:8px; width:96%; margin-bottom:5px")
+          .attr("style", "margin-left:8px; width:96%; margin-bottom:5px");
         let datebox = entrydiv.append("div").attr("class", "date-box");
+
         datebox.append("span").attr("class", "month")
           .text(function (d) {
             return d["time"].month;
           });
+          
         datebox.append("span").attr("class", "day")
           .text(function (d) {
             return d["time"].day;
