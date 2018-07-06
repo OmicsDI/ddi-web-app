222af129b4a359db7958acd3814d78ed53a8d816
xupan <hll3939092@gmail.com>
2018-07-06 10:34:45 +0800

    Delete all unit test files(*.spec.ts)

--- src/app/pipes/limit-dataset-numbers.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import {LimitDatasetNumbersPipe} from './limit-dataset-numbers.pipe';
-
-describe('LimitDatasetNumbersPipe', () => {
-    it('create an instance', () => {
-        const pipe = new LimitDatasetNumbersPipe();
-        expect(pipe).toBeTruthy();
-    });
-});
--- src/app/pipes/truncate.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import {TruncatePipe} from './truncate.pipe';
-
-describe('TruncatePipe', () => {
-    it('create an instance', () => {
-        const pipe = new TruncatePipe();
-        expect(pipe).toBeTruthy();
-    });
-});
