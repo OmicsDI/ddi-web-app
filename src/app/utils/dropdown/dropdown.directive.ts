c9c3cd300df590612cbf6bb5955d101af8263bd0
Tu Manh Vu <glmanhtu@gmail.com>
2018-07-10 14:01:27 +0100

    OF-66 Rewrite ng2-dropdown

---
+++ src/app/utils/dropdown/dropdown.directive.ts
@@ -0,0 +1,69 @@
+import {Directive, ElementRef, ContentChild, Output, EventEmitter, Input} from '@angular/core';
+import {DropdownNotClosableZoneDirective} from './dropdown-not-closable-zone.directive';
+
+@Directive({
+    selector: '[appDropdown]',
+    exportAs: 'appDropdown'
+})
+export class DropdownDirective {
+
+    // -------------------------------------------------------------------------
+    // Inputs / Outputs
+    // -------------------------------------------------------------------------
+
+    @Input()
+    dropdownToggle = true;
+
+    @Input()
+    dropdownFocusActivate = false;
+
+    @Output()
+    onOpen = new EventEmitter();
+
+    @Output()
+    onClose = new EventEmitter();
+
+    // -------------------------------------------------------------------------
+    // Properties
+    // -------------------------------------------------------------------------
+
+    @ContentChild(DropdownNotClosableZoneDirective)
+    notClosableZone: DropdownNotClosableZoneDirective;
+
+    // -------------------------------------------------------------------------
+    // Constructor
+    // -------------------------------------------------------------------------
+
+    constructor(private elementRef: ElementRef) {
+    }
+
+    // -------------------------------------------------------------------------
+    // Public Methods
+    // -------------------------------------------------------------------------
+
+    open() {
+        const element: HTMLElement = this.elementRef.nativeElement;
+        element.classList.add('open');
+        this.onOpen.emit(undefined);
+    }
+
+    close() {
+        const element: HTMLElement = this.elementRef.nativeElement;
+        element.classList.remove('open');
+        this.onClose.emit(undefined);
+    }
+
+    isOpened() {
+        const element: HTMLElement = this.elementRef.nativeElement;
+        return element.classList.contains('open');
+    }
+
+    isInClosableZone(element: HTMLElement) {
+        if (!this.notClosableZone) {
+            return false;
+        }
+
+        return this.notClosableZone.contains(element);
+    }
+
+}
