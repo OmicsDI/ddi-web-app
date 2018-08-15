16090dca911e616359cd10a9c09d9c5629774d02
xupan <hll3939092@gmail.com>
2018-08-15 18:21:34 +0800

    lint problem

--- src/test/modules/dataset/dataset.component.spec.ts
+++ src/test/modules/dataset/dataset.component.spec.ts
@@ -152,7 +152,8 @@ describe('DatasetComponent', () => {
             const reanalysis = El.querySelector('#reanalysis');
             const connections = El.querySelector('#connections');
             // console.log(h4.textContent);
-            // expect(h4.textContent).toContain('Anion-Exchange Chromatography of Tryptic and Phosphopeptides: WAX vs. SAX and AEX vs. ERLIC');
+            // expect(h4.textContent).toContain('Anion-Exchange
+            // Chromatography of Tryptic and Phosphopeptides: WAX vs. SAX and AEX vs. ERLIC');
             expect(abstract.textContent).toContain('At a pH > 5,  phosphopeptides have two negative charges per resid' +
                 'ue and are well-retained in anion-exchan' +
                 'ge chromatography.  However, the peptides' +
