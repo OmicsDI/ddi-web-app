bf54c8a5edaad1969269be4d4fd9c327903c8161
Tu Manh Vu <glmanhtu@gmail.com>
2019-06-10 17:20:42 +0100

    Fixed Oauth while list problem

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -4,6 +4,7 @@ export const environment = {
     webServiceUrl : 'http://dev.omicsdi.org/ws/',
     userServiceUrl: 'http://dev.omicsdi.org/profilews/api/',
     userServiceCookiePath: '/profilews/',
+    whileList: ['localhost:4200', 'dev.omicsdi.org', 'omicsdi.org', 'stage.omicsdi.org'],
     schemaServiceUrl: 'http://wwwdev.ebi.ac.uk/Tools/omicsdi/ws/seo',
     thorUrl: 'https://www.ebi.ac.uk/europepmc/hubthor/api/dataclaiming/'
 };
