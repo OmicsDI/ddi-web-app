b06d491f2eed474e92ae4748f112e3363d1993e3
Tu Manh Vu <glmanhtu@gmail.com>
2019-06-19 15:02:07 +0100

    Added local kubernetes configuration

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     baseHref: '/',
-    webServiceUrl : 'http://dev.omicsdi.org/ws/',
-    userServiceUrl: 'http://dev.omicsdi.org/profilews/api/',
+    webServiceUrl : 'http://127.0.0.1:30080/ws/',
+    userServiceUrl: 'http://127.0.0.1:30090/profilews/api/',
     userServiceCookiePath: '/profilews/',
     whileList: ['localhost:4200', 'dev.omicsdi.org', 'omicsdi.org', 'stage.omicsdi.org'],
     schemaServiceUrl: 'http://wwwdev.ebi.ac.uk/Tools/omicsdi/ws/seo',
