2fc23d2fdb5736a57a5ef8f0717aaf1cc6aa9a0f
Gaurhari <gaurharidass@gmail.com>
2019-09-20 14:31:29 +0100

    updated kubernetes for testing installation

---
+++ src/environments/environment.hari.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: true,
+    baseHref: '/',
+    webServiceUrl: 'https://stage.omicsdi.org/ws/',
+    userServiceUrl: 'https://stage.omicsdi.org/profilews/api/',
+    userServiceCookiePath: '/profilews/',
+    schemaServiceUrl: 'https://stage.omicsdi.org/ws/seo',
+    whileList: ['omicsdi.org', 'stage.omicsdi.org', 'www.omicsdi.org'],
+    thorUrl: 'https://www.ebi.ac.uk/europepmc/thor/api/dataclaiming/'
+};
