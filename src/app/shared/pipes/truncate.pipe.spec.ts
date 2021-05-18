88a7e2dd23a6e0ee7e77cd3787f221991d1f014a
Robert Petryszak <info@datasome.co.uk>
2021-05-18 19:17:52 +0100

    Added more tests

--- src/app/shared/pipes/truncate.pipe.spec.ts
+++ src/app/shared/pipes/truncate.pipe.spec.ts
@@ -12,4 +12,15 @@ describe('Pipe: Default', () => {
         expect(pipe.transform('Lorem ipsum dolor sit amet, consectetur adipiscing elit', '15', '...'))
             .toBe('Lorem ipsum dol...');
     });
+
+    it('Truncate 15 characters - no ellipsis', () => {
+        expect(pipe.transform('Lorem ipsum dolor sit amet, consectetur adipiscing elit', '15', ''))
+            .toBe('Lorem ipsum dol');
+    });
+
+
+    it('Truncate 50 characters - with highlighting', () => {
+        expect(pipe.transform('The human genome contains many thousands of long noncoding RNAs (lncRNAs). While several studies', '50', '...', 'and;noncoding'))
+            .toBe('...contains many thous<b>and</b>s of long <b>noncoding</b> RNAs (lncRNAs). W...');
+    });
 });
