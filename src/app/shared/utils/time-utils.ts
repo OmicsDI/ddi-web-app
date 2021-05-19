87579a31a257b3b6d120b6afa245477f34e43e68
Robert Petryszak <info@datasome.co.uk>
2021-05-19 09:12:42 +0100

    Added year to dates to dataset entries in 'Latest Datasets' panel

--- src/app/shared/utils/time-utils.ts
+++ src/app/shared/utils/time-utils.ts
@@ -1,10 +1,11 @@
 export class TimeUtils {
-    static getMonthDay(dateString: string): string {
+    static getMonthDayYear(dateString: string): string {
         const month_names_short = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
         const month_int = parseInt(dateString.substr(4, 2), 10);
         const day_int = parseInt(dateString.substr(6, 2), 10);
         const month = month_names_short[month_int - 1];
-        return month + ' ' + day_int + ' ';
+        const yearShort = dateString.substr(2, 2)
+        return month + ' ' + day_int + ' \'' + yearShort + " ";
     }
 
     static getMonthName(month: number): string {
