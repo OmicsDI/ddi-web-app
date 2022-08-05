936e30b9207d767a92a929691c1d128d455aa0a6
Gaurhari <gaurharidass@gmail.com>
2022-08-05 09:58:39 +0100

    updated string datatype

--- src/app/shared/services/validate.service.ts
+++ src/app/shared/services/validate.service.ts
@@ -17,12 +17,12 @@ export class ValidateService extends BaseService {
         super();
     }
 
-    public getValidateErrors(formData:FormData): Observable<Set<string>> {
+    public getValidateErrors(formData:FormData): Observable<string[]> {
         //alert(formData.get("isError"));
         const upload$ =  this.http.post(this.appConfig.getValidateUrl(), formData, {
             reportProgress: true,
             observe: 'body'
-        }).pipe(map(x => this.extractData<Set<string>>(x)));
+        }).pipe(map(x => this.extractData<string[]>(x)));
 
         return upload$;
     }
